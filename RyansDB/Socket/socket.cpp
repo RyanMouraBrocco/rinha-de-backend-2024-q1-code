e1471b1a317bd7a13cffe859fa34c6a006fabf55
RyanMouraBrocco <ryanmourabrocco@gmail.com>
2024-03-08 20:51:05 -0300

    move folder and added socket class

---
+++ RyansDB/Socket/socket.cpp
@@ -0,0 +1,64 @@
+#include "socket.hpp"
+
+Socket::Socket(u_int16_t port)
+{
+    int socketListener = CreateSocketListenerToIpv4();
+    if (socketListener < 0)
+    {
+        m_status = SocketStatus::Fail;
+        return;
+    }
+
+    int bindResult = BindSocketListenerToPort(socketListener, port);
+    if (bindResult < 0)
+    {
+        m_status = SocketStatus::Fail;
+        return;
+    }
+}
+
+Socket::~Socket()
+{
+    if (m_status == SocketStatus::Opened)
+        close(m_socketListener);
+}
+
+void Socket::Listen()
+{
+    if (m_status == SocketStatus::Fail)
+        return;
+
+    if (listen(m_socketListener, SOMAXCONN) == -1)
+    {
+        // std::cerr << "Can't listen";
+        m_status = SocketStatus::Fail;
+    }
+}
+
+int Socket::CreateSocketListenerToIpv4()
+{
+    int socketListener = socket(AF_INET, SOCK_STREAM, 0);
+    // if (socketListener == -1)
+    // {
+    //     std::cerr << "Error to create socket listener";
+    //     return -1;
+    // }
+
+    return socketListener;
+}
+
+int Socket::BindSocketListenerToPort(int socketListener, u_int16_t port)
+{
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+
+    if (bind(socketListener, (sockaddr *)&hint, sizeof(hint)) == -1)
+    {
+        // std::cerr << "Can't bind to IP/port";
+        return -2;
+    }
+
+    return 1;
+}
---
+++ RyansDB/main.cpp
@@ -0,0 +1,20 @@
+// C++ libraries
+#include <iostream>
+#include <string>
+
+// C libraries
+#include <string.h>
+
+int main()
+{
+    
+    
+    char buf[4096];
+    while (true)
+    {
+        
+    }
+
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
